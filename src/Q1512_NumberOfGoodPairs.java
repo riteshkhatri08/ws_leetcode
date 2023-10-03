1bc7d332bcdb6a5dc00ea39a959e42052b5826df
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-10-03 23:43:41 +0530

    Solved Q1512 -  Number Of Good Pairs

---
+++ src/Q1512_NumberOfGoodPairs.java
@@ -0,0 +1,53 @@
+import java.util.HashMap;
+
+public class Q1512_NumberOfGoodPairs {
+
+    public static void main(String[] args) {
+        int[] nums = new int[] { 1, 2, 3, 1, 1, 3 };
+        System.out.println(new Solution().numIdenticalPairs(nums));
+    }
+}
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int totalCount = 0;
+        HashMap<Integer, Pair> numCount = new HashMap<Integer, Pair>();
+        Pair current;
+        for (int i = 0; i < nums.length; i++) {
+            current = numCount.getOrDefault(nums[i], new Pair());
+            current.count++;
+            if (current.count == 1) {
+                numCount.put(nums[i], current);
+                continue;
+            }
+            if (current.count == 2) {
+                current.gp = 1;
+            } else {
+                current.gp = current.gp + current.count - 1;
+            }
+
+        }
+
+        for (Pair p : numCount.values()) {
+            totalCount += p.gp;
+        }
+
+        return totalCount;
+
+    }
+
+}
+
+class Pair {
+    int gp;
+    int count;
+
+    public Pair() {
+        this.gp = 0;
+        this.count = 0;
+    }
+
+    public String toString() {
+        return "[ gp=" + this.gp + ", count=" + this.count + "]";
+    }
+}
