cf1413593adc964bad6fcfbe82a8c85dbc8cc92f
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-10-04 02:22:48 +0530

    Added Q2038_RemoveColoredPieces

---
+++ src/Q2038_RemoveColoredPieces.java
@@ -0,0 +1,13 @@
+public class Q2038_RemoveColoredPieces {
+
+    public static void main(String[] args) {
+        String colors = "AAABABB";
+        System.out.println(new Solution().winnerOfGame(colors));
+    }
+}
+
+class Solution {
+    public boolean winnerOfGame(String colors) {
+        return false;
+    }
+}
