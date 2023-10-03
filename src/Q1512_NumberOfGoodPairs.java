88183ec671d776bf7e57dfef831a06937ce5cefb
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-10-04 03:22:26 +0530

    Changed Solution class as inner class

--- src/Q1512_NumberOfGoodPairs.java
+++ src/Q1512_NumberOfGoodPairs.java
@@ -3,51 +3,52 @@ import java.util.HashMap;
 public class Q1512_NumberOfGoodPairs {
 
     public static void main(String[] args) {
+        Q1512_NumberOfGoodPairs obj = new Q1512_NumberOfGoodPairs();
         int[] nums = new int[] { 1, 2, 3, 1, 1, 3 };
-        System.out.println(new Solution().numIdenticalPairs(nums));
+        System.out.println(obj.new Solution().numIdenticalPairs(nums));
     }
-}
 
-class Solution {
-    public int numIdenticalPairs(int[] nums) {
-        int totalCount = 0;
-        HashMap<Integer, Pair> numCount = new HashMap<Integer, Pair>();
-        Pair current;
-        for (int i = 0; i < nums.length; i++) {
-            current = numCount.getOrDefault(nums[i], new Pair());
-            current.count++;
-            if (current.count == 1) {
-                numCount.put(nums[i], current);
-                continue;
+    class Solution {
+        public int numIdenticalPairs(int[] nums) {
+            int totalCount = 0;
+            HashMap<Integer, Pair> numCount = new HashMap<Integer, Pair>();
+            Pair current;
+            for (int i = 0; i < nums.length; i++) {
+                current = numCount.getOrDefault(nums[i], new Pair());
+                current.count++;
+                if (current.count == 1) {
+                    numCount.put(nums[i], current);
+                    continue;
+                }
+                if (current.count == 2) {
+                    current.gp = 1;
+                } else {
+                    current.gp = current.gp + current.count - 1;
+                }
+
             }
-            if (current.count == 2) {
-                current.gp = 1;
-            } else {
-                current.gp = current.gp + current.count - 1;
+
+            for (Pair p : numCount.values()) {
+                totalCount += p.gp;
             }
+
+            return totalCount;
 
         }
 
-        for (Pair p : numCount.values()) {
-            totalCount += p.gp;
+    }
+
+    class Pair {
+        int gp;
+        int count;
+
+        public Pair() {
+            this.gp = 0;
+            this.count = 0;
         }
 
-        return totalCount;
-
-    }
-
-}
-
-class Pair {
-    int gp;
-    int count;
-
-    public Pair() {
-        this.gp = 0;
-        this.count = 0;
-    }
-
-    public String toString() {
-        return "[ gp=" + this.gp + ", count=" + this.count + "]";
+        public String toString() {
+            return "[ gp=" + this.gp + ", count=" + this.count + "]";
+        }
     }
 }
--- src/Q2038_RemoveColoredPieces.java
+++ src/Q2038_RemoveColoredPieces.java
@@ -1,13 +1,18 @@
 public class Q2038_RemoveColoredPieces {
 
     public static void main(String[] args) {
-        String colors = "AAABABB";
-        System.out.println(new Solution().winnerOfGame(colors));
+        Q2038_RemoveColoredPieces obj = new Q2038_RemoveColoredPieces();
+        // "ABBBBBBBAAA"
+        // "AAABABBBABBBAAA"
+        String colors = "AAABABBBABBBAA";
+        System.out.println(obj.new Solution().winnerOfGame(colors));
     }
-}
 
-class Solution {
-    public boolean winnerOfGame(String colors) {
-        return false;
+    class Solution {
+        public boolean winnerOfGame(String cs) {
+
+            return false;
+        }
     }
+
 }
