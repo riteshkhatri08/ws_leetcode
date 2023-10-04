3929113aa704f0bc3e7bd32d2248b1cd448be52e
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-10-04 22:43:04 +0530

    Solved Q706_DesignHashMap

---
+++ src/Q706_DesignHashMap.java
@@ -0,0 +1,37 @@
+public class Q706_DesignHashMap {
+    public static void main(String[] args) {
+        Q706_DesignHashMap obj = new Q706_DesignHashMap();
+        
+        
+
+     String [] func =  new String []  { "put", "put", "get", "get", "put", "get", "remove", "get"};
+int [][] params = new int [] { {1, 1}, {2, 2}, {1}, {3}, {2, 1}, {2}, {2}, {2}};
+        MyHashMap map =  new MyHashMap();
+
+
+      
+    }
+
+
+}
+
+
+
+class MyHashMap {
+  
+    public MyHashMap() {
+        
+    }
+    
+    public void put(int key, int value) {
+        
+    }
+    
+    public int get(int key) {
+        
+    }
+    
+    public void remove(int key) {
+        
+    }
+}
