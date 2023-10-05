750bcb9550cccebf1c8894d174807c807cec1840
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-10-05 22:11:50 +0530

    Added Q229_MajorityElement2

---
+++ src/Q229_MajorityElement2.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class Q229_MajorityElement2 {
+
+    public static void main(String[] args) {
+        Q229_MajorityElement2 obj = new Q229_MajorityElement2();
+
+        int[] nums = { 3, 3, 3, 12, 12, 12, 2, 2, 2, 1, 3 };
+        System.out.println(obj.new Solution().majorityElement(nums));
+    }
+
+    class Solution {
+        public List<Integer> majorityElement(int[] nums) {
+            return null;
+        }
+    }
+}
