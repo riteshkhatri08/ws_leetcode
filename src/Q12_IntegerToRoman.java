7a8f473280064c51b37b08a92aab311a8a4d6ae2
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-10-06 19:38:07 +0530

    Added Q12_IntegerToRoman

---
+++ src/Q12_IntegerToRoman.java
@@ -0,0 +1,22 @@
+public class Q12_IntegerToRoman {
+
+    public static void main(String[] args) {
+        var obj = new Q12_IntegerToRoman();
+        int num = 1994;
+        String result = obj.new Solution().intToRoman(num);
+        System.out.println(result);
+    }
+
+    class Solution {
+
+        public String intToRoman(int num) {
+
+            String result = "";
+
+            return result;
+
+        }
+
+    }
+
+}
