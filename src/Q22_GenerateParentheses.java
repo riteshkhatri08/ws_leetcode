86ab07eaa7bed0592b19eb2138af45efddd216f6
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-10-06 20:17:33 +0530

    Added Q22_GenerateParentheses

---
+++ src/Q22_GenerateParentheses.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q22_GenerateParentheses {
+
+    public static void main(String[] args) {
+        var obj = new Q22_GenerateParentheses();
+        int num = 1994;
+        List<String> result = obj.new Solution().generateParenthesis(num);
+        System.out.println(result);
+    }
+
+    class Solution {
+        public List<String> generateParenthesis(int n) {
+
+            List<String> result = new ArrayList<String>();
+
+            return result;
+        }
+    }
+
+}
