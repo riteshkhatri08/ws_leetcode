5c7acbb08a711d2c35dd39a098798fa144183afc
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-10-06 14:06:12 +0530

    Solver Q343_IntegerBreak Optimal

--- src/Q343_IntegerBreak.java
+++ src/Q343_IntegerBreak.java
@@ -7,9 +7,60 @@ public class Q343_IntegerBreak {
     }
 
     class Solution {
-        public int integerBreak(int n) {
 
-            return -1;
+        // HashMap<Integer,Integer> maximums = new HashMap<Integer,Integer>();
+        int[] prevResults;
+        // public Solution(){
+        // maximums.put(2,1);
+        // maximums.put(3,2);
+        // }
+
+        public int integerBreak(int n) {
+            prevResults = new int[3 + n];
+            int result = 0;
+            prevResults[2] = 1;
+            prevResults[3] = 2;
+            if (prevResults[n] != 0) {
+                return prevResults[n];
+                // if (n == 2){
+                // return 1;
+                // } else if (n == 3){
+                // return 2;
+            } else {
+
+                int div = 2;
+                int quo, rem;
+                double current, max = 0;
+
+                for (; div < n; div++) {
+                    quo = n / div;
+                    rem = n - (div * quo);
+                    if (rem == 1) {
+                        // System.out.println("BEFORE " + rem + "," + quo + "," + div);
+                        quo--;
+                        rem = rem + div;
+                        // System.out.println("AFTER " + rem + "," + quo + "," + div);
+                    }
+                    current = rem == 0 ? Math.pow(div, quo) : Math.pow(div, quo) * rem;
+                    max = current > max ? current : max;
+                }
+                result = (int) max;
+                prevResults[n] = result;
+
+            }
+
+            return result;
+
         }
+
     }
 }
+
+// 15 - = 243
+
+// 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+// 2^8 + 1
+// 3
+
+// 1,1,1,1,1,1,1,1,1,1,1,1,1,2
+// 1,1,1,2,2,2,2,2,2
