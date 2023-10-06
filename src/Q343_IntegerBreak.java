459bea78419c9d6a375d3d90145949359606c108
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-10-06 14:05:03 +0530

    Added Q343_IntegerBreak

---
+++ src/Q343_IntegerBreak.java
@@ -0,0 +1,15 @@
+public class Q343_IntegerBreak {
+
+    public static void main(String[] args) {
+        var obj = new Q343_IntegerBreak();
+        int num = 13;
+        System.out.println(obj.new Solution().integerBreak(num));
+    }
+
+    class Solution {
+        public int integerBreak(int n) {
+
+            return -1;
+        }
+    }
+}
