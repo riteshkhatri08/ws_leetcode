b20185bdbd336b39c9a60ef5ce5d912123eed982
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-11-10 03:50:32 +0530

    Optimize Q50_Pow

--- src/Q50_Pow.java
+++ src/Q50_Pow.java
@@ -1,5 +1,3 @@
-import java.util.HashMap;
-
 public class Q50_Pow {
     public static void main(String[] args) {
         var result = new Q50_Pow().new Solution().myPow(2, -2147483648);
@@ -8,32 +6,43 @@ public class Q50_Pow {
     }
 
     class Solution {
-        HashMap<Double, Double> memory = new HashMap<Double, Double>(20);
-        {
-            memory.put(0.0, 1.0);
-        }
+        // HashMap<Long, Double> memory = new HashMap<Long, Double>(20);
+        // {
+        // memory.put(0L, 1.0);
+        // }
 
-        public double myPow(double x, int y) {
-            double n = (double) y;
+        public double myPow(double x, long n) {
+
             if (n < 0) {
                 x = 1 / x;
                 n = -1 * n;
             }
-            memory.put(1.0, x);
+            // memory.put(1L, x);
             return pow(x, n);
         }
 
-        private double pow(double x, double n) {
-            if (memory.containsKey(n)) {
-                return memory.get(n);
+        private double pow(double x, long n) {
+            if (n == 0) {
+                return 1;
             }
 
-            // System.out.println("new n = " + n / 2);
-            double res = pow(x, (int) n / 2) * pow(x, (int) n / 2);
-            if (n % 2 == 1) {
-                res = x * res;
+            if (n == 1) {
+                return x;
             }
-            memory.put(n, res);
+            // if (memory.containsKey(n)) {
+            // return memory.get(n);
+            // }
+
+            // System.out.println("new n = " + n / 2);
+            double res = pow(x, n / 2);
+
+            if (n % 2 == 1) {
+                res = x * res * res;
+            } else {
+                res = res * res;
+            }
+
+            // memory.put(n, res);
 
             return res;
 
