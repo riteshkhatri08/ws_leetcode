d72dd56bc38f16e0e77560a8b8fa7d0efa8b0c8c
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-11-09 19:58:06 +0530

    Optimize Q1921_EliminateMaximumNumberMonsters

--- src/Q1921_EliminateMaximumNumberMonsters.java
+++ src/Q1921_EliminateMaximumNumberMonsters.java
@@ -4,30 +4,37 @@ public class Q1921_EliminateMaximumNumberMonsters {
 
     public static void main(String[] args) {
 
-        int[] dist = new int[] {3,2,4};
-        int[] speed = new int[] {5,3,2};
+        int[] dist = new int[] { 3, 2, 4 };
+        int[] speed = new int[] { 5, 3, 2 };
 
-        var result = new Q1921_EliminateMaximumNumberMonsters().new Solution();
+        var result = new Q1921_EliminateMaximumNumberMonsters().new Solution().eliminateMaximum(dist, speed);
         System.out.println(result);
     }
 
     class Solution {
         public int eliminateMaximum(int[] dist, int[] speed) {
-
+            double[] time = new double[dist.length];
             for (int i = 0; i < dist.length; i++) {
-                double time = dist[i] / speed[i];
-                // System.out.println("time = " + time + " comp =" + (((int) time) + 0.5));
-                dist[i] = (time > ((int) time) + 0.5 )? (((int) time) + 1 ) : (int) time ;
-                // dist[i] = (int) Math.ceil(time);
+                // Calculate time for each monster to reach in same
+                // array to reduce space complexity
+                // System.out.println(dist[i] + " / " + speed[i] + " = " + ( (double)dist[i] /
+                // (double)speed[i]) );
+                time[i] = (double) dist[i] / (double) speed[i];
             }
-            Arrays.sort(dist);
-            // for (int a : dist) {
-            //     System.out.print(a + " ");
+
+            // Sort them
+            Arrays.sort(time);
+
+            // Print array
+            // for (double a : time){
+            // System.out.print(a +" ");
             // }
+
+            // Start counting
             int totalTime = 0;
             int counter = 1;
 
-            while (counter < dist.length && (dist[counter] - totalTime) > 1) {
+            while (counter < dist.length && (time[counter] - totalTime) > 1) {
                 totalTime++;
                 counter++;
             }
