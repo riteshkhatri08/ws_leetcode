21e16cd80ec9643b0b931dbf777c87b81271065e
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-11-15 02:50:26 +0530

    Optimized Q1930_UniqueLength3PalindromicSubsequences

--- src/Q1930_UniqueLength3PalindromicSubsequences.java
+++ src/Q1930_UniqueLength3PalindromicSubsequences.java
@@ -7,39 +7,60 @@ public class Q1930_UniqueLength3PalindromicSubsequences {
         System.out.println("ANSWER  " + result);
     }
 
+    class Pair {
+        int left, right = -1;
+
+        public Pair(int left) {
+            this.left = left;
+        }
+
+    }
+
     class Solution {
-        HashSet<String> hashSet = new HashSet<String>();
 
         public int countPalindromicSubsequence(String s) {
+
+            int c;
             char[] charray = s.toCharArray();
-            helper(0, 1, 2, charray);
-            return hashSet.size();
+            Pair[] charIndices = new Pair[26];
+            // int [] unqiueCharCount = new int [charray.length];
+
+            // int count = 0;
+            for (int i = 0; i < charray.length; i++) {
+                c = ((int) charray[i]) - 97;
+                if (charIndices[c] == null) {
+                    // count++;
+                    charIndices[c] = new Pair(i);
+                } else {
+                    charIndices[c].right = i;
+                }
+                // unqiueCharCount[i] = count;
+            }
+
+            // Arrays.stream(charray).forEach(c -> System.out.print(c + ", "));
+            // System.out.println("");
+
+            // Arrays.stream(unqiueCharCount).forEach(cx -> System.out.print(cx + ", "));
+            // Arrays.stream( charIndices).forEach(cx -> {if (cx!=null)
+            // {System.out.print("["+cx.left + "-" +cx.right+ "], ");}});
+            // System.out.println("");
+            int result = 0;
+            for (int i = 0; i < charIndices.length; i++) {
+                if (charIndices[i] != null && charIndices[i].right > -1) {
+                    HashSet<Character> uniques = new HashSet<Character>();
+                    for (int j = charIndices[i].left + 1; j < charIndices[i].right; j++) {
+                        uniques.add(charray[j]);
+                    }
+                    result += uniques.size();
+                }
+            }
+
+            return result;
         }
 
-        private void helper(int left, int middle, int right, char[] charray) {
-            // Base condition
-            // if (right == charray.length) {
-            // return;
-            // }
-            if (charray[left] == charray[right]) {
-                System.out.println("" + charray[left] + charray[middle] + charray[right]);
-                hashSet.add("" + charray[left] + charray[middle] + charray[right]);
-                return;
-            }
-
-            if (left < middle - 1) {
-                helper(left + 1, middle, right, charray);
-            }
-
-            if (middle < right - 1) {
-                helper(left, middle + 1, right, charray);
-            }
-
-            if (right < charray.length - 1) {
-                helper(left, middle, right + 1, charray);
-
-            }
-
-        }
     }
+    // left se character ka first index , right se character ka first index
+    // no of unqiue characters between index left and right
+    // no of unqiue characters is no of palindromes
+    // repeat for chracter at left + 1
 }
