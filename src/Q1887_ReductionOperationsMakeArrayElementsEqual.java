2baf16a6be9c123b55d0b1206279124d67d70f5b
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-11-20 02:41:38 +0530

    Optimize Q1887_ReductionOperationsMakeArrayElementsEqual

--- src/Q1887_ReductionOperationsMakeArrayElementsEqual.java
+++ src/Q1887_ReductionOperationsMakeArrayElementsEqual.java
@@ -1,8 +1,7 @@
-import java.util.Arrays;
-
 public class Q1887_ReductionOperationsMakeArrayElementsEqual {
     public static void main(String[] args) {
 
+        // [7,9,10,8,6,4,1,5,2,3]
         int[] nums = new int[] { 1, 2, 2, 4, 2, 9, 3, 3, 2, 1, 5 };
         var result = new Q1887_ReductionOperationsMakeArrayElementsEqual().new Solution().reductionOperations(nums);
         System.out.println("ANSWER = " + result);
@@ -12,21 +11,35 @@ public class Q1887_ReductionOperationsMakeArrayElementsEqual {
     class Solution {
 
         public int reductionOperations(int[] nums) {
-            Arrays.sort(nums);
-            int operations = 0;
-            {
-                int left = nums.length - 1;
-                int right = left;
-                while (left > -1) {
-                    // System.out.println("WINDOW SIZE = " + (nums.length - left));
-                    if (nums[left] != nums[right]) {
-                        operations += nums.length - left - 1;
-                        right = left;
-                    }
-                    left--;
+
+            int maxElement = 0;
+            // int minElementWithCount = Integer.MAX_VALUE;
+
+            // for (int a : nums) {
+            // maxElement = maxElement < a ? a : maxElement;
+            // minElementWithCount = a < minElementWithCount ? a : minElementWithCount;
+            // }
+
+            int[] bucket = new int[100001];
+            for (int a : nums) {
+                if (a > maxElement) {
+                    maxElement = a;
+                }
+                bucket[a]++;
+            }
+
+            int prev = 0;
+            int ops = 0;
+            for (int i = maxElement; i > 0; i--) {
+                if (bucket[i] > 0) {
+
+                    prev = prev + bucket[i];
+                    ops = ops + prev;
+
                 }
             }
-            return operations;
+            ops = ops - prev;
+            return ops;
         }
     }
 }
