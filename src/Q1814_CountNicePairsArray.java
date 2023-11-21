bf7daf84dc0063e1d7d3a217da3253219afd8bcf
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-11-22 01:13:09 +0530

    Optimize Q1814_CountNicePairsArray

--- src/Q1814_CountNicePairsArray.java
+++ src/Q1814_CountNicePairsArray.java
@@ -1,5 +1,5 @@
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Map;
 
 public class Q1814_CountNicePairsArray {
     public static void main(String[] args) {
@@ -9,59 +9,39 @@ public class Q1814_CountNicePairsArray {
     }
 
     class Solution {
-        HashMap<Integer, Integer> reverses = new HashMap<Integer, Integer>();
-
         public int countNicePairs(int[] nums) {
 
-            int irev = 0;
-            int jrev = 0;
-            String temp = "";
-            String temprev = "";
             long count = 0;
-            HashSet<String> countmap = new HashSet<String>();
-
+            int temp = 0;
+            Map<Integer, Integer> numcount = new HashMap<>();
             for (int i = 0; i < nums.length; i++) {
-                for (int j = i + 1; j < nums.length; j++) {
-
-                    temp = nums[i] + "_" + nums[j];
-                    if (countmap.contains(temp)) {
-                        count++;
-                        continue;
-                    }
-
-                    irev = rev(nums[i]);
-                    jrev = rev(nums[j]);
-                    temprev = irev + "_" + jrev;
-                    if (countmap.contains(temprev)) {
-                        count++;
-                        continue;
-                    }
-
-                    if ((nums[i] + jrev) == (nums[j] + irev)) {
-                        count++;
-                        countmap.add(temp);
-                        countmap.add(temprev);
-                    }
-                }
+                int diff = nums[i] - rev(nums[i]);
+                // System.out.println(nums[i]);
+                // count = count + numcount.getOrDefault(nums[i] ,0) ;
+                temp = numcount.getOrDefault(diff, 0);
+                count = (count + temp) % 1000000007;
+                numcount.put(diff, temp + 1);
+                // if (numcount.containsKey(diffs[i])){
             }
-            return (int) (count % 1000000007L);
+
+            // for (int c : numcount.values()){
+            // count = count + ( c * (c-1) / 2 );
+            // }
+            return (int) (count % 1000000007);
         }
 
         public int rev(int num) {
-            if (reverses.containsKey(num)) {
-                return reverses.get(num);
-            }
-            int temp = num;
             int numrev = 0;
 
             while (num > 0) {
-                numrev = (numrev * 10) + num % 10;
+                numrev = (numrev * 10) + (num % 10);
                 num = num / 10;
             }
-            reverses.put(temp, numrev);
-            reverses.put(numrev, temp);
-            // System.out.println("rev(" +temp+") = " + numrev);
+
             return numrev;
         }
     }
+
+    // i + rev(j) = j + rev(i)
+    // i - rev(i) + j - rev(j) = 0
 }
