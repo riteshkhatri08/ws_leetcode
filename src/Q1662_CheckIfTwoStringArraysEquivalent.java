b493d574ba82acf9466b4e0a45ced60c5e1c66f9
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-12-01 23:47:46 +0530

    Init Q1662_CheckIfTwoStringArraysEquivalent

---
+++ src/Q1662_CheckIfTwoStringArraysEquivalent.java
@@ -0,0 +1,9 @@
+public class Q1662_CheckIfTwoStringArraysEquivalent {
+    public static void main(String[] args) {
+        var result =  new Q1662_CheckIfTwoStringArraysEquivalent().new Solution();
+    }
+
+    class Solution {
+
+    }
+}
