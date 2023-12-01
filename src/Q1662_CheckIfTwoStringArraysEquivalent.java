f19da1e163405e7a1db519db7d3cdb970aea4d36
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-12-02 00:59:53 +0530

    Solved Q1662_CheckIfTwoStringArraysEquivalent

--- src/Q1662_CheckIfTwoStringArraysEquivalent.java
+++ src/Q1662_CheckIfTwoStringArraysEquivalent.java
@@ -1,9 +1,44 @@
 public class Q1662_CheckIfTwoStringArraysEquivalent {
     public static void main(String[] args) {
-        var result =  new Q1662_CheckIfTwoStringArraysEquivalent().new Solution();
+        String[] word1 = new String[] { "ab", "c" }, word2 = new String[] { "a", "bc" };
+        var result = new Q1662_CheckIfTwoStringArraysEquivalent().new Solution().arrayStringsAreEqual(word1, word2);
+        System.out.println("ANSWER = " + result);
     }
 
     class Solution {
+        public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+            int p1 = 0;
+            int p2 = 0;
+            char[] w1, w2;
+            int i = 0, j = 0;
+            for (; i < word1.length && j < word2.length;) {
 
+                w1 = word1[i].toCharArray();
+                w2 = word2[j].toCharArray();
+
+                while (p1 < w1.length && p2 < w2.length) {
+                    // System.out.println("Comparing = " + w1[p1] + " =? " + w2[p2]);
+                    if (w1[p1++] != w2[p2++]) {
+                        return false;
+                    }
+                }
+
+                if (p1 >= w1.length) {
+                    p1 = 0;
+                    i++;
+                }
+
+                if (p2 >= w2.length) {
+                    p2 = 0;
+                    j++;
+                }
+            }
+            if (i >= word1.length && j >= word2.length) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }
     }
 }
