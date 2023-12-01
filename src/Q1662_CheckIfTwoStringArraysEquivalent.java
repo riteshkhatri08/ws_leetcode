3c407983f28012106894589f08fba382bbe47ee4
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-12-02 01:27:28 +0530

    OPtimize Q1662_CheckIfTwoStringArraysEquivalent

--- src/Q1662_CheckIfTwoStringArraysEquivalent.java
+++ src/Q1662_CheckIfTwoStringArraysEquivalent.java
@@ -7,38 +7,38 @@ public class Q1662_CheckIfTwoStringArraysEquivalent {
 
     class Solution {
         public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-            int p1 = 0;
-            int p2 = 0;
-            char[] w1, w2;
-            int i = 0, j = 0;
-            for (; i < word1.length && j < word2.length;) {
 
-                w1 = word1[i].toCharArray();
-                w2 = word2[j].toCharArray();
-
-                while (p1 < w1.length && p2 < w2.length) {
-                    // System.out.println("Comparing = " + w1[p1] + " =? " + w2[p2]);
-                    if (w1[p1++] != w2[p2++]) {
-                        return false;
+            boolean result = false;
+            {
+                StringBuilder b1 = new StringBuilder();
+                StringBuilder b2 = new StringBuilder();
+                int i = 0;
+                for (; i < word1.length && i < word2.length; i++) {
+                    {
+                        b1.append(word1[i]);
+                        b2.append(word2[i]);
                     }
                 }
 
-                if (p1 >= w1.length) {
-                    p1 = 0;
-                    i++;
+                for (; i < word1.length; i++) {
+                    b1.append(word1[i]);
                 }
 
-                if (p2 >= w2.length) {
-                    p2 = 0;
-                    j++;
+                for (; i < word2.length; i++) {
+                    b2.append(word2[i]);
                 }
-            }
-            if (i >= word1.length && j >= word2.length) {
-                return true;
-            } else {
-                return false;
-            }
 
+                if (b1.toString().equals(b2.toString())) {
+                    return true;
+                }
+                b1 = null;
+                b2 = null;
+                word1 = null;
+                word2 = null;
+                // System.gc();
+            }
+            return result;
         }
+
     }
 }
