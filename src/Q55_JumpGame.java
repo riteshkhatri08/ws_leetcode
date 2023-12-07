4f800b2d962100ec9bd6f741ac7e1ca8da9840a5
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-12-07 23:29:39 +0530

    Attempted Q55_JumpGame

--- src/Q55_JumpGame.java
+++ src/Q55_JumpGame.java
@@ -1,7 +1,7 @@
 public class Q55_JumpGame {
 
     public static void main(String[] args) {
-        int nums[] = new int[] { 3, 2, 1, 1, 4 };
+        int nums[] = new int[] { 2, 5, 0, 0 };
         var result = new Q55_JumpGame().new Solution().canJump(nums);
         System.out.println("RESULT = " + result);
 
@@ -14,14 +14,15 @@ public class Q55_JumpGame {
             // no of Choices = nums[i]
             // nums[i] == 0 return false
             // i == nums.length return true
-            mem = new int[nums.length];
+            // mem = new int [nums.length];
+
             return jumpKar(0, nums);
         }
 
         public boolean jumpKar(int index, int[] nums) {
-            if (mem[index] != 0) {
-                return mem[index] == -1 ? false : true;
-            }
+            // if (mem[index] != 0){
+            // return mem[index] == -1 ? false : true;
+            // }
             if (index == nums.length - 1) {
                 return true;
             }
@@ -30,22 +31,26 @@ public class Q55_JumpGame {
                 return false;
             }
 
-            // make sure new index < nums.length();
-            boolean result = false;
+            if (nums[index] + index >= nums.length) {
+                return true;
+            }
 
-            for (int i = nums[index]; i > 0; i--) {
+            // current index + 1
+            int nextPowerfulIndex = index + 1;
+            for (int i = 2; i <= nums[index]; i++) {
+                // System.out.println("npi=" + nextPowerfulIndex + " "+ nums[nextPowerfulIndex]
+                // + ", i=" + (index+i) + " " + nums[index+i] );
                 if (index + i < nums.length) {
-                    // System.out.println("BEFORE index=" + index +", new="+(index+i));
-                    result = jumpKar(index + i, nums);
-                    mem[index + i] = result == false ? -1 : 1;
-                    // System.out.println("AFTER index=" + index +", new="+(index+i)+",
-                    // res="+result);
-
-                    if (result == true) {
-                        break;
+                    // check the power at next LZ
+                    if (nums[index + i] > nums[nextPowerfulIndex]) {
+                        nextPowerfulIndex = index + i;
+                        System.out.println("npi=" + nextPowerfulIndex + " " + nums[nextPowerfulIndex] + ", i="
+                                + (index + i) + " " + nums[index + i]);
                     }
                 }
             }
+            // jump to most powerful index
+            boolean result = jumpKar(nextPowerfulIndex, nums);
 
             return result;
         }
