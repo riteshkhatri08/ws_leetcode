c9e2b80279df794ebf3aac75a2c44c360a2ac54f
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-12-10 13:47:07 +0530

    Optimize Q70_ClimbingStairs to use O(1) space

--- src/Q70_ClimbingStairs.java
+++ src/Q70_ClimbingStairs.java
@@ -4,31 +4,41 @@ public class Q70_ClimbingStairs {
         var result = new Q70_ClimbingStairs().new Solution().climbStairs(10);
         System.out.println("ANSWER = " + result);
     }
-    class Solution {
-        int [] dp;
-        public int climbStairs(int n) {
-            
-            dp =  new int [n+1];
-            dp[0] = 1;
-            dp[1] = 1;
 
+    class Solution {
+        int[] dp;
+
+        public int climbStairs(int n) {
+
+            // dp = new int[n + 1];
+            // dp[0] = 1;
+            // dp[1] = 1;
+            int a = 1;
+            int b =1;
+            int c = 1;
             // Instead of recursive approach we can use iteration here
-            for (int i = 2 ; i <= n ; i++){
-                dp[i] = dp[i-1] + dp[i-2];
+            for (int i = 2; i <= n; i++) {
+                // Since we are using only previous 2 values of i we dont need to store all
+                // values of i in dp
+                // this will consume o(1) instead of O(n) memory
+                // dp[i] = dp[i - 1] + dp[i - 2];
+                c = a+b;
+                a=b;
+                b=c;
             }
-        //   return  climbDown(n);
-        return dp[n];
+            // return climbDown(n);
+            // return dp[n];
+            return c;
         }
-    
-    
+
         // public int climbDown(int n){
-    
-        //     if (dp[n] != 0){
-        //         return dp[n];
-        //     }
-        //     dp[n-1] = climbDown(n-1);
-        //     dp[n-2] = climbDown(n-2);
-        //     return  dp[n-2] + dp[n-1];
+
+        // if (dp[n] != 0){
+        // return dp[n];
+        // }
+        // dp[n-1] = climbDown(n-1);
+        // dp[n-2] = climbDown(n-2);
+        // return dp[n-2] + dp[n-1];
         // }
     }
 }
