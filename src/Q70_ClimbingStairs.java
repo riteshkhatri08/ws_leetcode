dfda5860bbafa93610f7a4d38d860c9e60907e33
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-12-10 13:44:56 +0530

    Optimize Q70_ClimbingStairs to use iterative approach

--- src/Q70_ClimbingStairs.java
+++ src/Q70_ClimbingStairs.java
@@ -7,21 +7,28 @@ public class Q70_ClimbingStairs {
     class Solution {
         int [] dp;
         public int climbStairs(int n) {
+            
             dp =  new int [n+1];
             dp[0] = 1;
             dp[1] = 1;
-          return  climbDown(n);
-        }
-    
-    
-        public int climbDown(int n){
-    
-            if (dp[n] != 0){
-                return dp[n];
+
+            // Instead of recursive approach we can use iteration here
+            for (int i = 2 ; i <= n ; i++){
+                dp[i] = dp[i-1] + dp[i-2];
             }
-            dp[n-1] = climbDown(n-1);
-            dp[n-2] = climbDown(n-2);
-            return  dp[n-2] + dp[n-1];
+        //   return  climbDown(n);
+        return dp[n];
         }
+    
+    
+        // public int climbDown(int n){
+    
+        //     if (dp[n] != 0){
+        //         return dp[n];
+        //     }
+        //     dp[n-1] = climbDown(n-1);
+        //     dp[n-2] = climbDown(n-2);
+        //     return  dp[n-2] + dp[n-1];
+        // }
     }
 }
