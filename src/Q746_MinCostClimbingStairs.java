2d234fb8450162db701eac791aeae5e5293d545a
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-12-16 15:48:50 +0530

    Optimized Q746_MinCostClimbingStairs for space

--- src/Q746_MinCostClimbingStairs.java
+++ src/Q746_MinCostClimbingStairs.java
@@ -57,13 +57,10 @@ public class Q746_MinCostClimbingStairs {
 
     class Solution {
         public int minCostClimbingStairs(int[] cost) {
-            int dp[] = new int[cost.length];
-            dp[0] = cost[0];
-            dp[1] = cost[1];
-            for (int i = 2; i < cost.length; i++) {
-                dp[i] = Math.min(dp[i - 1] + cost[i], dp[i - 2] + cost[i]);
-            }
-            return Math.min(dp[cost.length - 2], dp[cost.length - 1]);
+            for (int i = 2; i < cost.length; i++)
+                cost[i] = Math.min(cost[i - 1], cost[i - 2]) + cost[i];
+
+            return Math.min(cost[cost.length - 2], cost[cost.length - 1]);
         }
     }
 
